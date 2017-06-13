ddd4ece3faa742f153a35c2fc02790b43c9a25c8
Vishal Maurya <vishalmr96@gmail.com>
2017-06-13 16:23:58 +0530

    Started php project

---
+++ webproject/feedback/feedback.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Feedback</title>
+</head>
+<body>
+	<h1>Feedback Form</h1>
+	<div>
+		<form method="post" action="feedback.php">
+			<label for="textarea">Enter Your Valuable</label>
+			<textarea name="feedback" id="feedback_input" cols="30" rows="10" placeholder="Enter your feedback here...."></textarea>
+			<label for="category">Choose The Category</label>
+			<input>
+		</form>
+	</div>
+</body>
+</html>
