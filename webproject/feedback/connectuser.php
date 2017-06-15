0b80b387f7e6cb7f208b3095403d5445516e8d3c
Vishal Maurya <vishalmr96@gmail.com>
2017-06-16 00:29:15 +0530

    add modal boxes in feedback form

--- webproject/feedback/connectuser.php
+++ webproject/feedback/connectuser.php
@@ -3,10 +3,9 @@ $ip="localhost";//ip address
 $db_user_name="root";//phpmyadmin username
 $db_password="";//phpmyadmin password
 $db="soc";//name of database
-$conn=mysqli_connect($ip,$db_user_name,$db_password,$db);//connecting to the database
-if(!$conn)
+$connect=mysqli_connect($ip,$db_user_name,$db_password,$db);//connecting to the database
+if(!$connect)
 {
     	 die( "error in connecting to database"); 
 }
-echo "hi";
 ?>
