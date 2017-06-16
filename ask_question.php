7f34a976f3f976bb090d4d07b944a154e9d37257
Vishal Maurya <vishalmr96@gmail.com>
2017-06-16 13:39:07 +0530

    add some styling to askquestion.php

--- ask_question.php
+++ ask_question.php
@@ -1,48 +1,74 @@
 <?php
-session_start();//to use the session's variable
+session_start();
 
 include 'function.php';
 include 'connectuser.php';
-?> 	
+?>  
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Ask a question</title>
-	<link rel="stylesheet" href="ask_question.css" type="text/css">
-	<script type="text/javascript"></script>
-</head>
-<body>
-<form action="ask_question.php" method="post">
+  <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/css/bootstrap-select.min.css">
+    <title>Ask a question</title>
+    <link rel="stylesheet" href="ask_question.css" type="text/css">
+    <!--script type="text/javascript"></script-->
+  </head>
+  <body>
 
+    <!-- heading of the file -->
+    <header>
+      <div class="jumbotron">
+        <div class="container">
+          <h1>Ask A Question</h1>
+        </div>
+      </div>
+    </header>
 
-<label>Username: 
-<?php              //to display the username from database
-$username=get_username();
-echo $username;
-?></label><br/>
-<textarea name="question" cols="50" rows="10"></textarea><br/><!--to post the question-->
-<input type="submit" name="post_question" value="Post"><br/><!--to submit the details to the databse-->
+    <div class="container">
+      <form action="ask_question.php" method="post">
 
-</form>
-<br/>
-<?php
-if(isset($_POST['question']) &&!empty($_POST['question']))   //to check whether the Post button is set and has been submitted
-{
-	$question=$_POST['question'];//to store the data in text area
-	  $my_id=$_SESSION['user_id'];//to store the user_id after logging in
-     $query="INSERT INTO question (user_id, question, upvotes, downvotes, score)
-     VALUES ('$my_id','$question','0','0','0')";//to insert the query
+      <div class="form-group">
+        <label for="ask_question">Username: 
+        <?php 
+        $username=get_username();
+        echo $username;
+        ?></label>
+        <textarea name="question" id="ask_question" cols="50" rows="10" class="form-control"></textarea>
+      </div>
 
-      if(mysqli_query($conn,$query))
-    { 
-        echo "added successfully";
-    }
-    else
-    {
-    	echo "error in posting question";
-    }
-}
-?>
-</body>
+      <div class="form-group">
+        <input type="submit" class="btn btn-warning" name="post_question" value="Post">
+      </div>
+      </form>
+    </div>
+
+    <hr>
+
+    <div class="container">
+      <?php
+      if(isset($_POST['question']) &&!empty($_POST['question']))
+      {
+        $question=$_POST['question'];
+          $my_id=$_SESSION['user_id'];
+           $query="INSERT INTO question (user_id, question, upvotes, downvotes, score)
+           VALUES ('$my_id','$question','0','0','0')";
+
+            if(mysqli_query($conn,$query))
+          { 
+              echo "added successfully";
+          }
+          else
+          {
+            echo "error in posting question";
+          }
+      }
+      ?>
+    </div>
+    <script src = "https://code.jquery.com/jquery.js"></script>
+      
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src = "js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/js/bootstrap-select.min.js"></script>
+  </body>
 </html>
 
--- connectuser.php
+++ connectuser.php
@@ -1,8 +1,8 @@
 <?php
 $ip="localhost";//ip address
 $db_user_name="root";//phpmyadmin username
-$db_password="1234";//phpmyadmin password
-$db="webproject";//name of database
+$db_password="";//phpmyadmin password
+$db="soc";//name of database
 $conn=mysqli_connect($ip,$db_user_name,$db_password,$db);//connecting to the database
 if(!$conn)
 {
