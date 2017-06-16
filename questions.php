f4fef75c7d5c831be16e622808a32f7aabffbf6f
Vishal Maurya <vishalmr96@gmail.com>
2017-06-16 23:23:12 +0530

    some styling to be aproved by pk

--- questions.php
+++ questions.php
@@ -12,120 +12,51 @@ include 'connectuser.php';
 	    <title>Questions!!</title>
 	    <link href="question_display.css" rel="stylesheet">
 	</head>
-	<body bgcolor="#E6E6FA">
-		<div class="container">	
+	<body>
+		<!-- header and heading -->
+		<div id="jumbotron">
+			<div class="container">
+				</span><h1 id="header_heading"><span class="glyphicon glyphicon-apple" id="logo"></span>Some Of The Valuables...</h1></span>
+			</div>
+		</div>
+
+		<div class="container">
 			<div class="row">
 				<div class="col-md-3">
 					<h3 class="text-danger"> This is for the side content....Below is a sample content </h3>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
+				</div>
 
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>					
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-					<p class="text-info"> learn C if you want to work in C, but probably not otherwise.
-
-					Most software development going on these days is not in C.
-
-					I love C, I work in C, but most people don’t need to learn it.
-
-					I say this a lot, to the point I bore myself:
-
-					Learn what you want to be g</p>
-				</div>				
-
-				<div class="col-md-9" bgcolor="#eee">
+				<div class="col-md-7" bgcolor="#eee">
 				    <?php
 				    $query="SELECT * FROM question";
 				    $result=mysqli_query($conn,$query);
 
 				    while($row=mysqli_fetch_array($result))
 					{
-						?>				   
-					   		<div class="container-fluid">
-						   		<div class="panel panel-default panel-info">
-									<div class="panel-heading" id="panel-heading">	
-									   <h3><?php 
-								       $name=get_user2($row[1]);
-									   echo "<a href='?user_id=$row[1]&quest_id=$row[0]'>$name</a>";
-									   ?></h3>
-									</div>
-									<div class="panel-body" id="ques_display"> 
-							   			<h3><?php echo $row[2];?></h3>
-							   		</div>
-								   	<div class="panel-footer" id="panel-footer">	
-								       	<form action="" method="post" >
-											<div class="form-group">						
-									       		<input type="submit" class="btn btn-primary" name="reply" value="Reply">		
-									       		<button type="submit" class="btn btn-default" name="like"><span class="glyphicon glyphicon-thumbs-up"></span></button>		       	
-									       		<button type="submit" class="btn btn-default" name="dislike"><span class="glyphicon glyphicon-thumbs-down"></span></button>
-								   			</div>
-								       	</form>
-								    </div>   	
-								</div>
-							</div>
+						?>									   
+					   		<div class="container-fluid"><hr id="hr_top"><div id="card">
+					   			<p class="help-block" id="heading_helpblock">Answer and Undiscovered Questions</p>
+							   	<h3 id="question_heading"><strong><?php
+						       	$name=get_user2($row[1]);
+							   	echo "<a href='?user_id=$row[1]&quest_id=$row[0]'>$name</a>";
+							   	?></strong></h3>
+								<p>
+					   			<blockquote><?php echo $row[2];?></blockquote>
+								</p>
+						       	<form action="" method="post" >
+									<div class="form-group">						
+							       		<button type="submit" class="btn btn-warning" name="reply"><strong>Reply</strong></button>
+							       		<button class="btn btn-link btn-md" id="simple"><strong id="upvote">Upvote</strong></button>
+							       		<button type="submit" class="btn btn-link" name="like"><span class="glyphicon glyphicon-thumbs-up"></span></button>		       	
+							       		<button type="submit" class="btn btn-link" name="dislike"><span class="glyphicon glyphicon-thumbs-down glyphicon-lg"></span></button>
+						   			</div>
+						       	</form>	</div>					     
+							</div>					
 					   
 					<?php
 						}
 							if(isset($_POST['like']) &&!empty($_POST['like'])) {
-							
+
 								$quest_id=$_GET['quest_id'];
 
 								$query1="SELECT upvotes FROM question WHERE question_id='$quest_id'";
