d613f5995b82c05abc93cf5b1579b350bf16165a
kpranav1998 <pranikhanna1998@gmail.com>
2017-06-16 15:49:17 +0530

    updaing

--- questions.php
+++ questions.php
@@ -18,35 +18,38 @@ include 'connectuser.php';
 	   <div class"questions">
 	   <h2><?php 
         $name=get_user2($row[1]);
-	   echo "<a href='?user_id=$row[1]?question_id=$row[0]'>$name</a>";
+	   echo "<a href='?user_id=$row[1]&quest_id=$row[0]'>$name</a>";
 	   ?></h2>
-	   <h3><?php echo $row[2];?></h1><br/>
+	   <h3><?php echo $row[2];?></h1><br/></h3>
       <form action="" method="post" >
       <input type="submit" name="reply" value="Reply">
       <input type="submit" name="like" value="Like">
       <input type="submit" name="dislike" value="Dislike">
       </form>
 
-	   <?php	
-	}
-	?>
-</body>
-</html>
+
+	   
 <?php
+}
 if(isset($_POST['like']) &&!empty($_POST['like']))
 {
-	$quest_id=$_GET['question_id'];
+	$quest_id=$_GET['quest_id'];
+	
 	$query1="SELECT upvotes FROM question WHERE question_id='$quest_id'";
 	$result=mysqli_query($conn,$query1);
 	$row=mysqli_fetch_array($result);
 	$likes=$row[0];
 	
 	$likes=$likes+1;
-	echo "$likes<br/>";
+	
 
-	$query2="UPDATE question SET upvotes='$likes' WHERE question_id='$row[0]'";
-	if(!mysqli_connect($conn,$query2))
+	$query2="UPDATE question SET upvotes='$likes' WHERE question_id='$quest_id'";
+	if(!mysqli_query($conn,$query2))
 	{
        echo "error";
 	}
 }
+?>
+</div>
+</body>
+</html>
