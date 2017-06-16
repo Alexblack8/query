34078717ddbc3d8ae576da5a7e6be985e4cd0bc6
kpranav1998 <pranikhanna1998@gmail.com>
2017-06-16 17:28:29 +0530

    updating thee files

--- questions.php
+++ questions.php
@@ -15,17 +15,19 @@ include 'connectuser.php';
     while($row=mysqli_fetch_array($result))
 	{
 		?>
-	   <div class"questions">
+	   <div class="questions">
 	   <h2><?php 
         $name=get_user2($row[1]);
+        echo $name;
 	   echo "<a href='?user_id=$row[1]&quest_id=$row[0]'>$name</a>";
 	   ?></h2>
-	   <h3><?php echo $row[2];?></h1><br/></h3>
+	   <h3><?php echo $row[2];?><br/></h3>
       <form action="" method="post" >
       <input type="submit" name="reply" value="Reply">
       <input type="submit" name="like" value="Like">
       <input type="submit" name="dislike" value="Dislike">
       </form>
+
 
 
 	   
@@ -34,7 +36,7 @@ include 'connectuser.php';
 if(isset($_POST['like']) &&!empty($_POST['like']))
 {
 	$quest_id=$_GET['quest_id'];
-	
+
 	$query1="SELECT upvotes FROM question WHERE question_id='$quest_id'";
 	$result=mysqli_query($conn,$query1);
 	$row=mysqli_fetch_array($result);
@@ -42,7 +44,7 @@ if(isset($_POST['like']) &&!empty($_POST['like']))
 	
 	$likes=$likes+1;
 	
-
+      
 	$query2="UPDATE question SET upvotes='$likes' WHERE question_id='$quest_id'";
 	if(!mysqli_query($conn,$query2))
 	{
