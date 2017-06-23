4b7d244bd7411c020256caa599b0d7791391bb01
Vishal Maurya <vishalmr96@gmail.com>
2017-06-23 18:55:01 +0530

    show reply of user in his/her profile

--- user_profile.php
+++ user_profile.php
@@ -14,6 +14,11 @@
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 		<link rel="stylesheet" href="question_display.css">
+		<style>
+			h1 {
+				border-bottom: 5px solid #8c1919;
+			}
+		</style>
 	</head>
 
 	<body>
@@ -33,7 +38,7 @@
 						<blockquote><p style="font-size: 17px;"><strong style="font-size: 1.2em;">This is User's description:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore dolorum, nihil molestias qui, in cum ipsum quis, doloremque delectus id eius dolorem libero magni! Aliquid quis consequuntur provident officia. </p></blockquote>
 					</div>
 				</div> <!--end row-->
-				<hr>
+				<hr style="height:1px;border:none;color:#333;background-color:#acadaf;">
 			</div> <!--end container-->
 		</header>
 		
@@ -44,6 +49,8 @@
 				
 				<!-- displaying the question asked by the user -->
 				<div class="col-md-7">
+					<h1 style="color: #8c1919;"><strong>QUESTIONS ASKED</strong></h1>
+
 					<?php
 						$question_dis_query = "SELECT question,question_heading AS q_heading FROM question WHERE user_id=".$get_user_id;
 						$question_dis_result = mysqli_query($conn, $question_dis_query);
@@ -58,10 +65,35 @@
 								<p><?php echo $question_dis_array['question']; ?></p>
 							</blockquote>
 						</div>
-						<hr>
+						<hr style="height:2px;border:none;color:#333;background-color:#eee;">
 
 						<?php
 						}
+						?>
+						
+						<h1 style="color: #8c1919;"><strong>ANSWERS GIVEN</strong></h1>
+						<?php
+						//showing replies
+						$reply_query = "SELECT reply,quest_id AS qID FROM replies WHERE user_id=".$get_user_id;
+						$reply_result = mysqli_query($conn,$reply_query);
+
+						while($reply_array = mysqli_fetch_array($reply_result)) {
+
+							$retrieve_q_heading = "SELECT question_heading AS q_heading FROM question WHERE question_id=".$reply_array['qID'];
+							$retrieve_q_heading_result = mysqli_query($conn,$retrieve_q_heading);
+							$retrieve_q_heading_array = mysqli_fetch_array($retrieve_q_heading_result);
+						?>
+							<div id="card">
+								<!-- reply heading -->
+								<h2 style="color: #d3970c;"><strong><?php echo $retrieve_q_heading_array['q_heading']; ?></strong></h2>
+								<!-- reply content -->
+								<blockquote>
+									<p><?php echo $reply_array['reply']; ?></p>
+								</blockquote>
+							</div>
+							<hr>
+						<?php 
+							}
 						?>
 				</div>
 			</div>
