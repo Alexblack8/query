ad85cfd405d67925d4b882f9fdc62728d134aabd
kpranav1998 <pranikhanna1998@gmail.com>
2017-06-25 17:48:18 +0530

    creating homepage

---
+++ homepage.php
@@ -0,0 +1,424 @@
+<!-- Designed By: Mohit Mohta & Suryaveer -->
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Student Gymkhana, IIT Indore</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!-- cards -->
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<script src="assets/js/modernizr.custom.js"></script>
+		<!-- login form -->
+		<link rel="stylesheet" type="text/css" href="login/css/normalize.css" />	
+		<link rel="stylesheet" type="text/css" href="login/css/modal.css" />
+		<script src="login/js/TweenMax.min.js"></script>
+		<script src="login/js/jquery.min.js"></script>
+		<!-- /// -->
+
+	</head>
+	<body>
+
+
+		<!-- Header -->
+			<header id="header" class="preview">
+				<div class="inner">
+					<div class="content" >
+						<h1>Student Gymkhana</h1>
+						<h2>We welcome you to the official<br />
+						page of Student Gymkhana, IIT Indore</h2>
+						<a href="#" class="button big alt" id="visit"><span style="line-height: 2.95em;">Visit</span></a>
+							
+					</div>
+					<a href="#" class="button hidden"><span>Check In</span></a>
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+				
+  <!-- <link href='http://fonts.googleapis.com/css?family=Raleway:400,200' rel='stylesheet' type='text/css'>   -->
+  <!-- <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> -->
+<style>
+  a {
+  color: white;
+  text-decoration: none;
+}
+.me {
+  width: 400px;
+  margin: 0px auto;
+}
+.me p,
+.me h1 {
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  text-align: center;
+}
+.me p {
+  font-weight: 200;
+}
+.me span {
+  font-weight: bold;
+}
+.social {
+  position: fixed;
+  top: 20px;
+  color: #fff;
+  font-family: 'Raleway', sans-serif;
+  z-index: 1;
+}
+.social ul {
+  padding: 0px;
+  -webkit-transform: translate(-260px, 0);
+  -moz-transform: translate(-260px, 0);
+  -ms-transform: translate(-260px, 0);
+  -o-transform: translate(-260px, 0);
+  transform: translate(-260px, 0);
+}
+.social ul li {
+  display: block;
+  margin: 5px;
+  background: rgba(0, 0, 0, .8);
+  color: white;
+  width: 300px;
+  text-align: right;
+  padding: 10px;
+  -webkit-border-radius: 0 30px 30px 0;
+  -moz-border-radius: 0 30px 30px 0;
+  border-radius: 0 30px 30px 0;
+  -webkit-transition: all 1s;
+  -moz-transition: all 1s;
+  -ms-transition: all 1s;
+  -o-transition: all 1s;
+  transition: all 1s;
+}
+.social ul li:hover {
+  -webkit-transform: translate(240px, 0);
+  -moz-transform: translate(240px, 0);
+  -ms-transform: translate(240px, 0);
+  -o-transform: translate(240px, 0);
+  transform: translate(240px, 0);
+  /*background: rgba(255, 255, 255, 0.4);*/
+   background: rgba(0, 0, 0, .8);
+}
+.social ul li:hover a {
+  color: white;
+}
+.social ul li:hover i {
+  color: white;
+  background: rgba(0, 0, 0, 0.9);
+  -webkit-transform: rotate(360deg);
+  -moz-transform: rotate(360deg);
+  -ms-transform: rotate(360deg);
+  -o-transform: rotate(360deg);
+  transform: rotate(360deg);
+  -webkit-transition: all 1s;
+  -moz-transition: all 1s;
+  -ms-transition: all 1s;
+  -o-transition: all 1s;
+  transition: all 1s;
+}
+.social ul li i {
+  margin-left: 10px;
+  color: #000;
+  background: #fff;
+  padding: 10px;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  font-size: 20px;
+  background: #ffffff;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -ms-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+}
+  </style>
+<nav class="social">
+          <ul>
+              <li>
+                <a href="http://www.fluxus.in" target="_blank" style="float:left;" ><h1 style="margin:0;">And, the best of the days are<br> the days of<br>Fluxus!</a>
+                <img src="images/fluxus_logo.png" height="55px">
+              </li> 
+          </ul>
+</nav>				<div class="inner">
+					<div class="columns">
+						<ul class="grid cs-style-1">
+							<li>
+								<figure>
+									<div class="front-small">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/whyiiti.png"></center>
+										<h3>About IIT-Indore</h3>
+									</div>
+										<a href="pages/about-iiti.php">
+											<figcaption>
+												<h3 style="margin-top: 50px;">About</h3>
+												<span>IIT INDORE<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+												
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+
+							<li>
+								<figure>
+									<div class="front-big"><br><br><br><br><br><br>
+										<center><img style="height:50px;width:50px;" src="images/tech.png"></center>
+
+										<h3>Technical Clubs<br>IIT Indore</h3>
+									</div>
+										<a href="pages/tech-clubs.php">
+											<figcaption>
+												<h3>Technical Club</h3>
+												<span>IIT INDORE<br>
+													........<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+
+							<li>
+								<figure>
+									<div class="front-small">
+										<br><br>
+										<center><img style="height:50px;width:200px;" src="images/sesc.png"></center>
+										<h3 style="font-size:1em;">Student Entrepreneurship Support Cell</h3>
+									</div>
+										<a href="http://www.sesc.in/cie/">
+											<figcaption>
+												<h3 style="margin-top: 50px;">Student Entrepreneurship Support Cell</h3>
+												<span>IIT INDORE<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+												
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-big"><br><br><br><br><br><br>
+										<center><img style="height:50px;width:50px;" src="images/office.png"></center>
+										<h3> Gymkhana<br> office</h3>
+									</div>
+										<a href="pages/office.php">
+											<figcaption>
+												<h3>Office</h3>
+												<span>Gymkhana<br>
+													IIT INDORE<br><br>
+													Details about structure<br>
+													different secretaries, Senators<br>
+													and their reponsibilities<br><br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-small">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/news.png"></center>
+										<h3>Campus News and Event calendar</h3>
+									</div>
+										<a href="pages/news.php">
+											<figcaption>
+												<h3>Campus News and Event calendar<br></h3>
+												<span>IIT INDORE<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-small" style="background-color: #523F2C;">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/social-works.png"></center>
+										<h3>Social Works</h3>
+									</div>
+										<a href="pages/social-works.php">
+											<figcaption>
+												<h3 style="margin-top: 50px;">Social Woks</h3>
+												<span>IIT INDORE<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+												
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							
+							<li>
+								<figure>
+									<div class="front-big"><br><br><br><br><br><br>
+										<center><img style="height:50px;width:50px;" src="images/hostel.png"></center>
+
+										<h3>Hostel<br>IIT indore</h3>
+									</div>
+										<a href="pages/hostel.php">
+											<figcaption>
+												<h3>Hostel</h3>
+												<span>IIT INDORE<br><br>
+													About hostel facilities<br>
+													contact details and gallery<br><br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-small">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/placements.png"></center>
+										<h3>Placement updates</h3>
+									</div>
+										<a href="http://placement.iiti.ac.in/">
+											<figcaption>
+												<h3>Placement updates</h3>
+												<span>IIT INDORE<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-small" style="background-color: #523F2C;">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/alumni-cell.png"></center>
+										<h3>Alumni Cell</h3>
+									</div>
+										<a href="pages/alumni-cell.php">
+											<figcaption>
+												<h3 style="margin-top: 50px;">Alumni cell</h3>
+												<span>IIT INDORE<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+												
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+
+							<li>
+								<figure>
+									<div class="front-small">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/achievements.png"></center>
+
+										<h3>Achievements & <br> Photo gallery</h3>
+									</div>
+										<a href="pages/achievements.php">
+											<figcaption>
+												<h3 style="margin-top: 50px;">Achievements <br> Photo gallery</h3>
+												<span>IIT INDORE<br>
+
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-big"><br><br><br><br><br><br>
+										<center><img style="height:50px;width:50px;" src="images/cultural.png"></center>
+										<h3>Cultural Clubs<br> IIT INDORE</h3>
+									</div>
+										<a href="pages/cult-clubs.php">
+											<figcaption>
+											<h3>Cultural Clubs</h3>
+												<span>IIT INDORE<br>
+													...........<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<div class="front-small">
+										<br><br>
+										<center><img style="height:50px;width:50px;" src="images/reach-us.png"></center>
+										<h3>Reach Us<br>address@iit-indore</h3>
+
+									</div>
+										<a href="pages/reach-us.php">
+											<figcaption>
+												<h3>Reach Us</h3>
+												<span>address@iit-indore<br>
+												 	<span style="color : #7cc576;">Click for more details...</span>
+											</figcaption>
+											</figcaption>
+										</a>
+								</figure>
+							</li>
+
+						</ul>	
+					
+					</div>
+				</div>
+	
+			</div>
+
+			<!--  cards end ///////////////// -->
+
+
+		<!-- Footer -->
+		<style>
+	
+/*	@media screen and (max-width: 1250px) {
+	 #header_logo  img{
+		width: 400px;
+	}
+}*/
+@media screen and (max-width: 480px) {
+	 #header_logo img{
+		width: 260px;
+	}
+}
+
+</style>
+			<footer id="footer">
+				<a href="#" class="info fa fa-info-circle"><span>About</span></a>
+				<div class="inner">
+					<div class="content">
+						<center><div id="header_logo"><img  src="http://iiti.ac.in/images/logo.png" width="440px"></div>
+						<strong><h3>Student Gymkhana, IIT Indore</h3></strong></center>
+						<!-- <p>Ensuring the welfare of students in and outside the campus.</p> -->
+					
+
+					</div>
+					<!-- login form -->
+							<div class="content_login" id="login_form">
+									<a href="http://people.iiti.ac.in/~studentgym/login/session_var.php" class="open-modal" style="background-color:#fff;text-decoration:none; color:#2c3f52;">Login</a>								</div>
+					<div class="copyright">
+						<h3>Follow Us</h3>
+						<ul class="icons">
+							<li><a href="https://www.facebook.com/studentgym" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="https://www.facebook.com/studentgym" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					
+						</ul>
+						&copy;Designed By : <a href="https://www.linkedin.com/in/suryaveer/" target="_blank">Suryaveer Singh</a> and <a href="https://www.linkedin.com/in/mohtamohit/" target="_blank">Mohit Mohta</a>
+					</div>
+				</div>
+			</footer>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+			
+	</body>
+</html>
