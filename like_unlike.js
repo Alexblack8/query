825446adc1f9a342b1286a201114690007b6fe25
Vishal Maurya <vishalmr96@gmail.com>
2017-06-25 17:38:58 +0530

    improve like and unlike in questions

--- like_unlike.js
+++ like_unlike.js
@@ -4,14 +4,21 @@ $(document).ready(function() {
 	$(".like, .dislike").click(function() {
 		var id = this.id; // getting button id
 		var split_id = id.split("-");
-		var click_like = split_id[2];
+		var user_id = split_id[2];
 		var text = split_id[0];
 		var question_id = split_id[1];
+		var type = 0;
+		if(text == 'like')
+			type = 1;
+		else
+			type = 0;
+
+		alert("the text is: "+text+"the user_id is: "+user_id+"the question_id is: "+question_id+"the type is: "+type); 
 		// ajax request
 		$.ajax({
 			url: 'like_unlike.php',
 			type: 'POST',
-			data: {question_id:question_id,click_like:click_like},
+			data: {question_id:question_id,type:type,user_id:user_id},
 			dataType: 'json',
 			success: function(data) {
 				var likes = data['likes'];
