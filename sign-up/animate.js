fe556207beebf696a333a34ce911c208cfae6401
Vishal Maurya <vishalmr96@gmail.com>
2017-07-05 20:40:16 +0530

    made a fully functional login form

--- sign-up/animate.js
+++ sign-up/animate.js
@@ -6,16 +6,21 @@ $(document).ready(function() {
 				left: "440px",
 				right: null
 			});
-			$("input[name*='Name'],.text-area").fadeOut("fast").slideUp("fast");
+			$("input[name*='name'],.text-area,#email").fadeOut("fast").slideUp("fast");
+			$("#username").fadeIn("fast").slideDown("fast");
 			$(".formButton").text("Login");
+			$("#desktop_form").attr("action","../userlogin.php");
+			$("#desktop_form").attr("novalidate","");
 		}
 		else if(id == 'signup') {
 			$("form").animate({
 				left: null,
 				right: "440px"
-			},"swing");
-			$("input[name*='Name'],.text-area").fadeIn("fast").slideDown("fast");
+			});
+			$("input[name*='name'],.text-area,#email").fadeIn("fast").slideDown("fast");
 			$(".formButton").text("Sign Up");
+			$("#desktop_form").attr("action","../userregister.php");
+			$("#desktop_form").removeAttr("novalidate");
 		}
 	});
 
