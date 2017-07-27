743b2e6c1f305b851a28101cabea0dbd8b8078a6
kpranav1998 <pranikhanna1998@gmail.com>
2017-07-28 00:59:32 +0530

    updating database

--- webproject_dump.sql
+++ webproject_dump.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: webproject
 -- ------------------------------------------------------
--- Server version	5.7.18-0ubuntu0.17.04.1
+-- Server version	5.7.18-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,10 +29,10 @@ CREATE TABLE `feedback` (
   `upvotes` int(11) DEFAULT NULL,
   `downvotes` int(11) DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
-  `tags` int(11) NOT NULL,
+  `tags` varchar(128) NOT NULL,
   `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`feedback_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=69348 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `feedback` (
 
 LOCK TABLES `feedback` WRITE;
 /*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
-INSERT INTO `feedback` VALUES (1,1,'hellos',7,3,NULL,2,'2017-06-20 15:10:23');
+INSERT INTO `feedback` VALUES (1,1,'All IITs have many options for minor program but our iit lacks them .If the administration could look into it would lead to more diversified learning',NULL,NULL,NULL,'academics','2017-07-27 18:28:08'),(15542,1,'<p>It would be of great help if we could get a seperate rooom for table tennis because really hard to play along with the gym.</p>\r\n',NULL,NULL,NULL,'sports','2017-07-27 18:56:20'),(69347,1,'<p>The Mess food has only degraded in quality inspite of the fee hike.I think we deserve a better meal than the present given to us.If the general scretary could take serious note into this matter it woud be of great help</p>\r\n',NULL,NULL,NULL,'mess','2017-07-27 18:30:18');
 /*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,8 +57,9 @@ CREATE TABLE `like_unlike` (
   `user_id` int(11) DEFAULT NULL,
   `question_id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
+  `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +68,7 @@ CREATE TABLE `like_unlike` (
 
 LOCK TABLES `like_unlike` WRITE;
 /*!40000 ALTER TABLE `like_unlike` DISABLE KEYS */;
-INSERT INTO `like_unlike` VALUES (1,3,5,0),(2,6,5,1),(3,3,1,1),(4,7,5,1),(5,1,5,1),(6,1,1,1),(7,8,5,0),(8,8,1,1),(9,9,6,1),(10,9,7,0);
+INSERT INTO `like_unlike` VALUES (1,1,40112,1,'2017-07-27 18:33:31');
 /*!40000 ALTER TABLE `like_unlike` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +85,7 @@ CREATE TABLE `like_unlike_reply` (
   `reply_id` int(11) DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +94,6 @@ CREATE TABLE `like_unlike_reply` (
 
 LOCK TABLES `like_unlike_reply` WRITE;
 /*!40000 ALTER TABLE `like_unlike_reply` DISABLE KEYS */;
-INSERT INTO `like_unlike_reply` VALUES (1,8,1,1),(2,8,4,1),(3,8,2,1),(4,8,3,1),(5,9,7,0),(6,9,5,0),(7,9,6,1);
 /*!40000 ALTER TABLE `like_unlike_reply` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,7 +113,7 @@ CREATE TABLE `notificationlike` (
   `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `ref_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,7 +122,7 @@ CREATE TABLE `notificationlike` (
 
 LOCK TABLES `notificationlike` WRITE;
 /*!40000 ALTER TABLE `notificationlike` DISABLE KEYS */;
-INSERT INTO `notificationlike` VALUES (3,1,1,0,'reply','2017-06-26 10:05:07',0),(4,1,1,0,'reply','2017-06-26 10:05:10',0),(33,1,1,1,'like','2017-06-26 10:39:56',0),(34,1,1,0,'question','2017-06-26 10:23:10',2),(35,1,1,0,'question','2017-06-26 10:23:35',2),(36,1,1,0,'question','2017-06-26 10:24:17',6),(37,1,1,0,'','2017-06-26 09:56:33',1),(38,1,1,0,'reply','2017-06-26 09:56:33',1),(39,1,1,0,'feedback','2017-06-26 09:56:33',1),(40,1,1,0,'reply','2017-06-26 09:56:33',5),(41,1,1,0,'reply','2017-06-26 09:56:33',5),(42,1,1,0,'reply','2017-06-26 09:56:33',5),(43,1,1,0,'reply','2017-06-26 09:56:33',5),(44,1,1,0,'reply','2017-06-26 09:56:33',5),(45,1,1,0,'reply','2017-06-26 09:56:33',5),(46,1,1,0,'reply','2017-06-26 09:56:33',1),(47,1,1,0,'reply','2017-06-26 09:56:33',3),(48,1,3,1,'reply','2017-06-25 15:31:28',5),(49,1,1,0,'reply','2017-06-26 09:56:33',1),(50,1,1,0,'reply','2017-06-26 09:56:33',1),(51,1,3,1,'reply','2017-06-25 15:35:05',5),(52,1,1,0,'reply','2017-06-26 09:56:33',1),(53,1,1,0,'reply','2017-06-26 09:56:33',1),(54,1,1,0,'reply','2017-06-26 09:56:33',5),(55,1,1,0,'reply','2017-06-26 09:56:33',6),(56,1,1,0,'reply','2017-06-26 09:56:33',6),(57,2,1,0,'reply','2017-06-26 09:56:33',6);
+INSERT INTO `notificationlike` VALUES (1,1,1,1,'reply','2017-07-27 18:24:26',1),(2,1,1,1,'question','2017-07-27 18:33:31',40112),(3,1,1,1,'reply','2017-07-27 18:33:39',1),(4,1,1,1,'reply','2017-07-27 19:18:45',60244);
 /*!40000 ALTER TABLE `notificationlike` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +144,7 @@ CREATE TABLE `question` (
   `question_heading` text,
   `tags` varchar(128) NOT NULL,
   PRIMARY KEY (`question_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=78673 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,7 +153,7 @@ CREATE TABLE `question` (
 
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
-INSERT INTO `question` VALUES (6,1,'weewewq',11,0,1.3815037962693,'2017-06-26 07:26:38',NULL,'transport'),(7,1,'sdasdada',0,0,0,'2017-06-26 07:26:43',NULL,'transport');
+INSERT INTO `question` VALUES (1,1,'Why was query created when we have an option of posting feedbacks and questions on the official mail?',0,0,0,'2017-07-27 18:21:47','What is the purpose of query?','Others'),(40112,1,'Even though gst was applied it would lead to only a marginal increase in price but the fee hike was exorbitant.Please look into this matter.',0,0,0,'2017-07-27 18:31:51','Why was the mess fees hiked to such an extent?','mess'),(60244,1,'',0,0,0,'2017-07-27 19:15:36','How does query work?','others');
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,7 +174,7 @@ CREATE TABLE `replies` (
   `score` double DEFAULT NULL,
   `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`reply_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,7 +183,7 @@ CREATE TABLE `replies` (
 
 LOCK TABLES `replies` WRITE;
 /*!40000 ALTER TABLE `replies` DISABLE KEYS */;
-INSERT INTO `replies` VALUES (1,5,1,'This is my first reply',NULL,NULL,0,'2017-06-25 15:35:05'),(2,1,1,'thsi is reply for sports section',NULL,NULL,0,'2017-06-25 15:35:24'),(3,1,1,'this is for quest. 1',NULL,NULL,0,'2017-06-25 15:35:37'),(4,5,1,'this is for quest 5',NULL,NULL,0,'2017-06-25 15:35:49'),(5,6,1,'dsasadsadsda',NULL,NULL,0,'2017-06-26 07:35:19'),(6,6,1,'dsasadsadsda',NULL,NULL,0,'2017-06-26 07:35:23'),(7,6,2,'eqwewqeewweeq',NULL,NULL,0,'2017-06-26 09:42:10');
+INSERT INTO `replies` VALUES (1,1,1,'<p>We have been noticing that there was no proper platform for posting questions and feedbacks ,and the official mail did not live upto expectations.Thus query helps in all these matters.</p>',NULL,NULL,0,'2017-07-27 18:24:26'),(3,60244,1,'<p>Anybody can reply to questions and get to know feedbacks.The 4&nbsp;most popular posts will be displayed under questions and feedbacks and will be displayed according to their popularity.All the other posts can be viewed under various&nbsp;categories .People will get notifications if anybody replies to their posts or likes them.So for any doubt or feedbacks just QUERY IT</p>',NULL,NULL,0,'2017-07-27 19:18:45');
 /*!40000 ALTER TABLE `replies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,7 +195,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `first_name` varchar(128) NOT NULL,
   `last_name` varchar(128) NOT NULL,
   `user_name` varchar(128) NOT NULL,
@@ -203,7 +203,7 @@ CREATE TABLE `user` (
   `email_id` varchar(128) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,7 +212,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'pranav','khanna','pranavk28','1234','1234@gmail.com','Hello my name is pranav.It is my dream to create the greatest company in the world which benifits my country and the human race in total.\r\n\r\nMY second dream is to join the bjp and the rss.'),(2,'pkoqwwkdl','lwldwkl','hey','1234','ranav@GMAIL,COM',''),(3,'someone','ghhgjhj','wtf','1234','hjhjhj',''),(6,'Vishal','Maurya','vishal','1234','vishalmry@gmail.com',''),(7,'dass','sadsdaads','hello','1234','dasdsads',' dsadsasdasdsda'),(8,'frustated','admi','admi','1234','hello@gmail.comm',' i am fucked up'),(9,'sahaj','k','sahaj','1234','sahaj@g.com',' ');
+INSERT INTO `user` VALUES (1,'pranav','khanna','pranavk28','1234','pranikhanna1998@gmail.com','entrepreneurship is my dream ');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -225,4 +225,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-26 17:16:41
+-- Dump completed on 2017-07-28  0:55:12
